b0a2582999ffe39553cb33a785e115745eacdeff
suikelei <sui.kelei@htnova.com>
2019-11-09 10:15:05 +0800

    编译错误

--- src/main/java/com/htnova/scaffold/SubmarineApplication.java
+++ src/main/java/com/htnova/scaffold/SubmarineApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 
 @SpringBootApplication
