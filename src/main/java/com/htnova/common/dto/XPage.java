329a4c1c5c2dd13c60bda26e7e0475498e631640
suikelei <sui.kelei@htnova.com>
2019-12-13 16:29:04 +0800

    clean code

--- src/main/java/com/htnova/common/dto/XPage.java
+++ src/main/java/com/htnova/common/dto/XPage.java
@@ -1,12 +1,9 @@
 package com.htnova.common.dto;
 
-import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.core.metadata.OrderItem;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.google.common.collect.Lists;
-import com.htnova.common.base.BaseMapStruct;
 import lombok.Data;
-import org.mapstruct.factory.Mappers;
 
 @Data
 public class XPage<T> extends Page<T> {
@@ -23,21 +20,4 @@ public class XPage<T> extends Page<T> {
         //初始化排序
         this.setOrders(Lists.newArrayList(defaultOrderItem));
     }
-
-    /**
-     * page 转 dto
-     */
-    public static <D,E> XPage<D> toDto(IPage<E> iPage, Class<? extends BaseMapStruct<D,E>> mapStruct) {
-        XPage<D> resultPage = new XPage<>();
-        resultPage.setTotal(iPage.getTotal());
-        resultPage.setSize(iPage.getSize());
-        resultPage.setCurrent(iPage.getCurrent());
-        resultPage.setOrders(iPage.orders());
-        resultPage.setOptimizeCountSql(iPage.optimizeCountSql());
-        resultPage.setSearchCount(iPage.isSearchCount());
-        BaseMapStruct<D,E> mapper = Mappers.getMapper(mapStruct);
-        resultPage.setRecords(mapper.toDto(iPage.getRecords()));
-        return resultPage;
-    }
-
 }
