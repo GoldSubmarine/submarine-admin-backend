17c4a842695f07068b32f74b361667e44492d71d
GoldSubmarine <997462392@qq.com>
2020-08-04 14:31:31 +0800

    权限删除bug fix

--- src/main/java/com/htnova/system/manage/service/PermissionService.java
+++ src/main/java/com/htnova/system/manage/service/PermissionService.java
@@ -1,7 +1,7 @@
 package com.htnova.system.manage.service;
 
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.htnova.common.base.BaseEntity;
 import com.htnova.system.manage.dto.PermissionDto;
@@ -50,15 +50,15 @@ public class PermissionService extends ServiceImpl<PermissionMapper, Permission>
 
     @Transactional
     public void deletePermission(long id) {
-        // 删除角色的关联表
-        rolePermissionService.remove(
-                new LambdaQueryWrapper<>(new RolePermission())
-                        .eq(RolePermission::getPermissionId, id));
         // 删除子级和自身
         List<Permission> permissionList = super.lambdaQuery().like(Permission::getPids, id).list();
         List<Long> ids =
                 permissionList.stream().map(BaseEntity::getId).collect(Collectors.toList());
         ids.add(id);
         super.removeByIds(ids);
+        // 删除角色的关联表
+        rolePermissionService.remove(
+                Wrappers.lambdaQuery(new RolePermission())
+                        .in(RolePermission::getPermissionId, ids));
     }
 }
