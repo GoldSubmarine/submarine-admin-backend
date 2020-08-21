e7a99cc45e7fcf0d10b814a9851b870d2bc34af6
GoldSubmarine <997462392@qq.com>
2020-08-21 13:48:39 +0800

    参数校验的错误处理,fix #18

--- src/main/java/com/htnova/common/exception/ExceptionTranslate.java
+++ src/main/java/com/htnova/common/exception/ExceptionTranslate.java
@@ -3,9 +3,8 @@ package com.htnova.common.exception;
 import com.htnova.common.constant.ResultStatus;
 import com.htnova.common.dto.Result;
 import javax.servlet.ServletException;
+import javax.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 
@@ -18,6 +17,7 @@ public class ExceptionTranslate {
         while (error instanceof ServletException && error.getCause() != null) {
             error = error.getCause();
         }
+        // 这种方式针对RequestResponseBodyMethodProcessor 有效，也就是在 @Validated @RequestBody XXXDTO 的方式有效
         BindingResult bindingResult = extractBindingResult(error);
         if (bindingResult != null) {
             result =
@@ -26,10 +26,9 @@ public class ExceptionTranslate {
                     bindingResult.hasErrors() ? ResultStatus.BIND_ERROR : ResultStatus.NO_ERROR
                 );
         } else {
-            if (error instanceof AuthenticationException) {
-                result = Result.build(HttpStatus.UNAUTHORIZED, ResultStatus.UNAUTHORIZED);
-            } else if (error instanceof AccessDeniedException) {
-                result = Result.build(HttpStatus.FORBIDDEN, ResultStatus.FORBIDDEN);
+            // 针对直接在controller里定义参数的情况 @NotNull String deviceId，需要直接捕获
+            if (error instanceof ConstraintViolationException) {
+                result = Result.build(HttpStatus.BAD_REQUEST, ResultStatus.BIND_ERROR);
             } else if (error instanceof ServiceException) {
                 result = Result.build((ServiceException) error);
             } else {
